b0186acba18ec0ecc884a7e4eb4d04c11456d84d
ipfsnut <40326908+ipfsnut@users.noreply.github.com>
2023-03-16 15:20:45 -0500

    Update [listingId].tsx

--- pages/listing/[listingId].tsx
+++ pages/listing/[listingId].tsx
@@ -63,7 +63,7 @@ const ListingPage: NextPage = () => {
         await marketplace?.direct.makeOffer(
           listingId, // The listingId of the listing we want to make an offer for
           1, // Quantity = 1
-          NATIVE_TOKENS[ChainId.Goerli].wrapped.address, // Wrapped Ether address on Goerli
+          NATIVE_TOKENS[ChainId.Polygon].wrapped.address, // Wrapped Ether address on Goerli
           bidAmount // The offer amount the user entered
         );
       }
